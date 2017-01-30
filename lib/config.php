85c0ba584b1b4e6543fd5a2a88d35515984d4396
Thecakeisgit <Thecakeisplaying@gmail.com>
2017-01-30 20:25:18 +0100

    Changed config and updated index.

--- lib/config.php
+++ lib/config.php
@@ -5,21 +5,31 @@ return Array(
 
       '<label>' => Array(
         '<price>',
-        '<duration>'
+        '<duration>',
+        '<flags>'
       ),
 
-   * With price as a float (Eg. 3.00) and duration in month (Eg. 1 => 1 Month).
+   * With price as a float (Eg. 3.00),
+   * duration in month (Eg. 1 => 1 Month),
+   * and the flags as a single or multiple chars (Eg. 'a' or 'zab').
+   * Note : Any flag will result in an execution of the script placed
+   * in 'lib/flags/<flag>.php'.
    */
   'payOptions' => Array(
     'currencyCode' => 'EUR',
     '1 Mois - Premium' => Array (
       '20.00',
-      '1'
+      '1',
+      'z'
     ),
     '1 An - Normal' => Array (
       '60.00',
-      '12'
+      '12',
+      ''
     )
   ),
+
+  /* FabManager API Key */
+  'apiKey' => '<apiKey>'
 );
 ?>
--- validateForm.php
+++ validateForm.php
@@ -1,5 +1,10 @@
 <?php
-  if(!empty($_POST)) {
-    echo lol;
+  /* If there is no POST data */
+  if(empty($_POST)) {
+    header("Location: ./"); // Redirect to form
+    exit;
   }
+
+  echo var_dump($_POST);
+
 ?>
