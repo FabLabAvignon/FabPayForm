6e82904e4042a16a11911e0cc23b3aa91d5400be
Thecakeisgit <Thecakeisplaying@gmail.com>
2017-01-30 19:14:41 +0100

    Changed duration value to month instead of days and added comment.

--- lib/config.php
+++ lib/config.php
@@ -1,18 +1,25 @@
 <?php
 return Array(
+  /*
+   * Define durations with price and label, basically this is the patern :
+
+      '<label>' => Array(
+        '<price>',
+        '<duration>'
+      ),
+
+   * With price as a float (Eg. 3.00) and duration in month (Eg. 1 => 1 Month).
+   */
   'payOptions' => Array(
     'currencyCode' => 'EUR',
-    'Monthly' => Array (
+    '1 Mois - Premium' => Array (
       '20.00',
-      '31'
+      '1'
     ),
-    'Yearly' => Array (
+    '1 An - Normal' => Array (
       '60.00',
-      '365'
+      '12'
     )
   ),
 );
-
-// On compte par mois et pas par jour !!
-// Voir la doc...
 ?>
