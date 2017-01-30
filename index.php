ae9141a967e67fa0fa17e3dd1e736d3635ecd2d6
doliG <guillaume.lodi@gmail.com>
2017-01-30 15:12:50 +0100

    Style grandement amélioré

--- index.php
+++ index.php
@@ -17,20 +17,23 @@
     <br>
     <div class="container">
         <h1>Oh, alors vous voulez devenir un maker, hein ?</h1>
-        <p>Vous y êtes presque ! Pour devenir adhérent, suivez les instructions
+        <br>
+        <p>Vous y êtes presque ! Pour devenir adhérent, remplissez le formulaire
         ci dessous...</p>
 
         <form class="" action="validateForm.php" method="POST">
 
             <!-- Requiered  -->
-            <h2>Donnez nous quelques informations à propos de vous</h2>
-
             <div class="sticker">
                 <h3>Le minimum syndical...</h3>
                 <div class="arrow-right"></div>
             </div>
             <p class="indice">Les informations ci-dessous sont obligatoire.</p>
-
+            <div class="input-block">
+                <label for="gender">Civilité</label><br>
+                <input type="radio" name="gender" value="homme"> Mr<br>
+                <input type="radio" name="gender" value="femme"> Mme
+            </div>
             <div class="input-block">
                 <label for="familyName">Nom</label><br>
                 <input type="text" name="familyName" placeholder="Votre nom de famille">
@@ -61,13 +64,9 @@
                 <h3>Vous êtes bavard ?</h3>
                 <div class="arrow-right"></div>
             </div>
-            <!-- Optionals  -->
+            <p class="indice">Les informations ci-dessous sont facultatives.</p>
 
-            <div class="input-block">
-                <label for="gender">Civilité</label><br>
-                <input type="radio" name="gender" value="homme"> Mr<br>
-                <input type="radio" name="gender" value="femme"> Mme
-            </div>
+            <!-- Optionals  -->
             <div class="input-block">
                 <label for="birthday">Date de naissance</label><br>
                 <input type="date" name="birthday" placeholder="jj/mm/aaaa">
